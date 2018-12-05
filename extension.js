6d357dcfc6678fd0149d90f1928ff7769a14650c
Josiah Coad <josiah@coad.net>
2018-12-05 00:21:09 -0600

    reformatting

--- extension.js
+++ extension.js
@@ -4,10 +4,10 @@ function sendMessage() {
     chrome.tabs.query({
         active: true,
         currentWindow: true
-    }, function (tabs) {
+    }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
             message: "ACTIVATE"
-        }, function (response) {
+        }, function(response) {
             if (response && response.message == SUCCESS) {
                 $("#activate").text("loaded");
             }
--- search.js
+++ search.js
@@ -2,79 +2,79 @@
 // is text matching the search term and then replace it with an anchor
 // tag with "link" and the search term as the clickable text.
 function searchAndReplace(node, search, link) {
-  var next;
+    var next;
 
-  if (node.nodeType === 1) {
-    // (Element node)
+    if (node.nodeType === 1) {
+        // (Element node)
 
-    if ((node = node.firstChild)) {
-      do {
-        // Recursively call traverseChildNodes
-        // on each child node
-        next = node.nextSibling;
-        searchAndReplace(node, search, link);
-      } while ((node = next));
+        if ((node = node.firstChild)) {
+            do {
+                // Recursively call traverseChildNodes
+                // on each child node
+                next = node.nextSibling;
+                searchAndReplace(node, search, link);
+            } while ((node = next));
+        }
+    } else if (node.nodeType === 3) {
+        // (Text node)
+        if (new RegExp(search, "gi").test(node.data)) {
+            // Do something interesting here
+            wrapMatchesInNode(node, search, link);
+        }
     }
-  } else if (node.nodeType === 3) {
-    // (Text node)
-    if (new RegExp(search, "gi").test(node.data)) {
-      // Do something interesting here
-      wrapMatchesInNode(node, search, link);
-    }
-  }
 }
 
 // wrap a text node in some DOM element
 function wrapMatchesInNode(textNode, search, link) {
-  var temp = document.createElement("div");
-  temp.innerHTML = textNode.data.replace(
-    new RegExp(search, "gi"),
-    `<a href="${link}">${search}</a>`
-  );
-  while (temp.firstChild) {
-    textNode.parentNode.insertBefore(temp.firstChild, null);
-  }
-  textNode.parentNode.removeChild(textNode);
+    var temp = document.createElement("div");
+    temp.innerHTML = textNode.data.replace(
+        new RegExp(search, "gi"),
+        `<a href="${link}">${search}</a>`
+    );
+    while (temp.firstChild) {
+        textNode.parentNode.insertBefore(temp.firstChild, null);
+    }
+    textNode.parentNode.removeChild(textNode);
 }
 
 function searchAndReplaceWithTooltip(node, data) {
-  var next;
-  var search = data.search
-  var link = data.link
-  if (node.nodeType === 1) {
-    // (Element node)
+    var next;
+    var search = data.search
+    var link = data.link
+    if (node.nodeType === 1) {
+        // (Element node)
 
-    if ((node = node.firstChild)) {
-      do {
-        // Recursively call traverseChildNodes
-        // on each child node
-        next = node.nextSibling;
-        searchAndReplaceWithTooltip(node, data);
-      } while ((node = next));
+        if ((node = node.firstChild)) {
+            do {
+                // Recursively call traverseChildNodes
+                // on each child node
+                next = node.nextSibling;
+                searchAndReplaceWithTooltip(node, data);
+            } while ((node = next));
+        }
+    } else if (node.nodeType === 3) {
+        // (Text node)
+        if (new RegExp(search, "gi").test(node.data)) {
+            // Do something interesting here
+            console.log(`found match on ${search}!`);
+            wrapMatchesInNodeWithTooltip(node, data);
+        }
     }
-  } else if (node.nodeType === 3) {
-    // (Text node)
-    if (new RegExp(search, "gi").test(node.data)) {
-      // Do something interesting here
-      console.log(`found match on ${search}!`);
-      wrapMatchesInNodeWithTooltip(node, data);
-    }
-  }
 }
 
 // wrap a text node in some DOM element
 function wrapMatchesInNodeWithTooltip(textNode, data) {
-  var search = data.search
-  var linkClass = search + "_stw_att"
-  var link = data.link
-  var temp = document.createElement("div");
-  temp.innerHTML = textNode.data.replace(
-    new RegExp(search, "gi"),
-    `<a class="${linkClass}" href="${link}">${search}</a>`
-  );
-  while (temp.firstChild) {
-    textNode.parentNode.insertBefore(temp.firstChild, null);
-  }
-  textNode.parentNode.removeChild(textNode);
-  InitializeTooltip(data, linkClass)
+    var search = data.search
+    var linkClass = search + "_stw_att"
+    var link = data.link
+    var temp = document.createElement("div");
+    temp.innerHTML = textNode.data.replace(
+        new RegExp(search, "gi"),
+        `<a class="${linkClass}" href="${link}">${search}</a>`
+    );
+    while (temp.firstChild) {
+        textNode.parentNode.insertBefore(temp.firstChild, null);
+    }
+    textNode.parentNode.removeChild(textNode);
+    InitializeTooltip(data, linkClass)
 }
