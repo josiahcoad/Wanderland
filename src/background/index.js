9e638921323d169dcb7d0b96c579011d72d34f4c
Chinmay Phulse <phulsechinmay@gmail.com>
2019-01-26 14:10:55 -0600

    Started adding context menu

--- src/background/index.js
+++ src/background/index.js
@@ -1,1 +1,20 @@
-// Currently empty, required for the Hot Extension Reloader
+listenerFunctionMap = {
+    selectionSTW : selectionTextListener
+}
+
+function selectionTextListener(info, tab){
+    selectionText = info.selectionText;
+    console.log(selectionText);
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({
+        id: "selectionSTW",
+        title: "See The World",
+        contexts: ["selection"],
+    });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    listenerFunctionMap[info.menuItemId](info, tab);
+})
