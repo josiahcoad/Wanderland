52871e10dcfc0e16e7ede76b3175864b418177cd
Josiah Coad <josiah@coad.net>
2019-02-03 03:15:46 -0600

    Added Feedback Form

---
+++ src/popup/components/feedbackForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Loader from './loader';
+
+const FeedbackForm = () => (
+    <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeYH2e5XDvBBQkPuTyqXxzn9Yrv4PBOlYo1U-t2wEiOrepb5g/viewform?embedded=true"
+        width="100%"
+        height="400"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+        title="Wandlerland Feedback Form"
+    >
+        <Loader />
+    </iframe>
+);
+
+export default FeedbackForm;
---
+++ src/popup/components/resultsPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
+import PopupMap from './popupMap';
+import ResultsList from './resultsList';
+import './popup.css';
+
+const ResultsPage = ({
+    placesScraped, setLastPlacesScraped, setSelectedPlace, selectedPlace,
+}) => (
+    <Grid>
+        {placesScraped.length !== 0 && (
+            <>
+                <Row>
+                    <Col xs={7}>
+                        {placesScraped.length !== 0 && (
+                            <PopupMap
+                                placesScraped={placesScraped}
+                                selectedPlace={selectedPlace}
+                                setSelectedPlace={setSelectedPlace}
+                                setLastPlacesScraped={setLastPlacesScraped}
+                            />
+                        )}
+                    </Col>
+                    <Col xs={4} xsOffset={1}>
+                        <ResultsList
+                            places={placesScraped}
+                            setPlaces={setLastPlacesScraped}
+                            setSelectedPlace={setSelectedPlace}
+                            selectedPlace={selectedPlace}
+                        />
+                    </Col>
+                </Row>
+            </>
+        )}
+    </Grid>
+);
+
+export default ResultsPage;
