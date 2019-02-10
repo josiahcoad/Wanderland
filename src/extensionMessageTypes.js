88d20a8407d85ad8e91e145aecc0595b69331648
Josiah Coad <josiah@coad.net>
2019-02-10 16:58:34 -0600

    merging master

--- src/extensionMessageTypes.js
+++ src/extensionMessageTypes.js
@@ -1,4 +1,5 @@
-export const TEXT_SCAN = 'TEXT_SCAN';
+export const LOOKUP_PLACE = 'LOOKUP_PLACE';
+export const SCAN_PARAGRAPH = 'SCAN_PARAGRAPH';
 export const PAGE_SCAN = 'PAGE_SCAN';
 export const PAGE_SCAN_SUCCESS = 'PAGE_SCAN_SUCCESS';
 export const PAGE_SCAN_FAILED = 'PAGE_SCAN_FAILED';
